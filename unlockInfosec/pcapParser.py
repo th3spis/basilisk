e8725a0bd64106104eb063fec243bdebee50eb20
th3sp1s <69475918+th3sp1s@users.noreply.github.com>
2020-09-16 11:30:23 +0200

    Update pcapParser.py

--- unlockInfosec/pcapParser.py
+++ unlockInfosec/pcapParser.py
@@ -4,6 +4,8 @@ import sys # ignore
 sys.path.insert(0,'.') # ignore
 
 #### USEFUL https://thepacketgeek.com/scapy/building-network-tools/part-04/
+#### USEFUL https://scapy.readthedocs.io/en/latest/usage.html
+
 
 def show_mac_address():
     packets = rdpcap(pcapPath.pcap)
