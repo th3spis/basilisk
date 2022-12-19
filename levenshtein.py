1f1c6a963399c53aa4b0babe7be791ce29632347
th3spis <69475918+th3spis@users.noreply.github.com>
2022-12-19 16:28:43 +0100

    Create levenshtein.py

---
+++ levenshtein.py
@@ -0,0 +1,17 @@
+# https://python-course.eu/applications-python/levenshtein-distance.php
+
+def LD(s, t):
+	if s == "":
+		return len(t)
+	if t == "":
+		return len(s)
+	if s[-1] == t[-1]:
+		cost = 0
+	else:
+		cost = 1
+	   
+	res = min([LD(s[:-1], t)+1,
+			     LD(s, t[:-1])+1, 
+			     LD(s[:-1], t[:-1]) + cost])
+	return res
+
